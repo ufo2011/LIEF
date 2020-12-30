81084c23383d8ad56b68589356f11f7f7ecfbd06
Romain Thomas <me@romainthomas.fr>
2020-12-30 15:00:34 +0100

    WIP

--- scripts/docker/linux-x86-64.sh
+++ scripts/docker/linux-x86-64.sh
@@ -1,24 +1,27 @@
-#!/usr/bin/sh
+#!/usr/bin/bash
 set -ex
 
 export CXXFLAGS='-ffunction-sections -fdata-sections -fvisibility-inlines-hidden -static-libstdc++ -static-libgcc'
 export CFLAGS='-ffunction-sections -fdata-sections -static-libstdc++ -static-libgcc'
 export LDFLAGS='-Wl,--gc-sections -Wl,--exclude-libs,ALL'
 
-$PYTHON_BINARY setup.py --ninja --lief-test build \
-  bdist_wheel --dist-dir wheel_stage
-
-auditwheel repair -w dist --plat manylinux1_x86_64 wheel_stage/*.whl
-
 mkdir -p build/linux-x86-64/static-release && mkdir -p build/linux-x86-64/shared-release
 pushd build/linux-x86-64/shared-release
 
 cmake ../../.. -GNinja \
   -DCMAKE_LINK_WHAT_YOU_USE=on \
+  -DLIEF_PE=off \
+  -DLIEF_ELF=off \
+  -DLIEF_ENABLE_JSON=off \
+  -DLIEF_OAT=off \
+  -DLIEF_DEX=off \
+  -DLIEF_VDEX=off \
+  -DLIEF_ART=off \
   -DBUILD_SHARED_LIBS=on \
   -DLIEF_PYTHON_API=off \
   -DLIEF_INSTALL_COMPILED_EXAMPLES=off \
-  -DCMAKE_BUILD_TYPE=Release
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX=$(pwd)/install
 
 ninja
 
@@ -27,15 +30,22 @@ pushd build/linux-x86-64/static-release
 
 cmake ../../.. -GNinja \
   -DCMAKE_LINK_WHAT_YOU_USE=on \
+  -DLIEF_PE=off \
+  -DLIEF_ELF=off \
+  -DLIEF_ENABLE_JSON=off \
+  -DLIEF_OAT=off \
+  -DLIEF_DEX=off \
+  -DLIEF_VDEX=off \
+  -DLIEF_ART=off \
   -DBUILD_SHARED_LIBS=off \
   -DLIEF_PYTHON_API=off \
   -DLIEF_INSTALL_COMPILED_EXAMPLES=on \
-  -DCMAKE_BUILD_TYPE=Release
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX=$(pwd)/install
 
 ninja
 
 popd
-
 pushd build/linux-x86-64
 cpack --config ../../cmake/cpack.config.cmake
 popd
@@ -43,4 +53,5 @@ popd
 /usr/bin/mv build/linux-x86-64/*.tar.gz build/
 ls -alh build
 
-chown -R 1000:1000 build dist wheel_stage
+# Fix permissions
+chown -R 1000:1000 /src/build/linux-x86-64
