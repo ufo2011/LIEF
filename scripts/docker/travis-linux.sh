d0cc3157af31cc5685a24088c82e64bf22f1d7d3
Romain Thomas <me@romainthomas.fr>
2020-12-30 15:42:40 +0100

    WIp

--- scripts/docker/travis-linux.sh
+++ scripts/docker/travis-linux.sh
@@ -10,37 +10,4 @@ $PYTHON_BINARY setup.py --ninja --lief-test build \
 
 auditwheel repair -w dist --plat manylinux1_x86_64 wheel_stage/*.whl
 
-mkdir -p build/linux-x86-64/static-release && mkdir -p build/linux-x86-64/shared-release
-pushd build/linux-x86-64/shared-release
-
-cmake ../../.. -GNinja \
-  -DCMAKE_LINK_WHAT_YOU_USE=on \
-  -DBUILD_SHARED_LIBS=on \
-  -DLIEF_PYTHON_API=off \
-  -DLIEF_INSTALL_COMPILED_EXAMPLES=off \
-  -DCMAKE_BUILD_TYPE=Release
-
-ninja
-
-popd
-pushd build/linux-x86-64/static-release
-
-cmake ../../.. -GNinja \
-  -DCMAKE_LINK_WHAT_YOU_USE=on \
-  -DBUILD_SHARED_LIBS=off \
-  -DLIEF_PYTHON_API=off \
-  -DLIEF_INSTALL_COMPILED_EXAMPLES=on \
-  -DCMAKE_BUILD_TYPE=Release
-
-ninja
-
-popd
-
-pushd build/linux-x86-64
-cpack --config ../../cmake/cpack.config.cmake
-popd
-
-/bin/mv build/linux-x86-64/*.tar.gz build/
-ls -alh build
-
 chown -R 1000:1000 build dist wheel_stage
--- scripts/windows/package_sdk.py
+++ scripts/windows/package_sdk.py
@@ -11,6 +11,8 @@ LIEF_SRC = CWD / ".." / ".."
 BUILD_PATH = LIEF_SRC / "build"
 BUILD_STATIC_PATH = BUILD_PATH / "static-release"
 BUILD_SHARED_PATH = BUILD_PATH / "shared-release"
+CPACK_CONFIG_PATH = (LIEF_SRC / "cmake" / "cpack.config.cmake").resolve()
+CPACK_BIN = pathlib.WindowsPath("C:/Program Files/CMake/bin/cpack.exe").as_posix()
 
 BUILD_PATH.mkdir(exist_ok=True)
 BUILD_STATIC_PATH.mkdir(exist_ok=True)
@@ -22,32 +24,35 @@ env.update(ninja_env)
 
 cmake_config_static = [
     "-G", "Ninja",
+    "-DCMAKE_BUILD_TYPE=Release",
+    "-DBUILD_SHARED_LIBS=off",
     "-DLIEF_PYTHON_API=off",
     "-DLIEF_INSTALL_COMPILED_EXAMPLES=on",
     "-DLIEF_USE_CRT_RELEASE=MT",
-    "-DCMAKE_BUILD_TYPE=Release",
 ]
 
 cmake_config_shared = [
     "-G", "Ninja",
+    "-DCMAKE_BUILD_TYPE=Release",
     "-DBUILD_SHARED_LIBS=on",
     "-DLIEF_PYTHON_API=off",
     "-DLIEF_INSTALL_COMPILED_EXAMPLES=off",
     "-DLIEF_USE_CRT_RELEASE=MT",
-    "-DCMAKE_BUILD_TYPE=Release",
 ]
 
 
 build_args = ['--config', 'Release']
 
 
-configure_cmd = ['cmake', LIEF_SRC.absolute().as_posix()] + cmake_config_shared
+configure_cmd = ['cmake', LIEF_SRC.resolve().as_posix()] + cmake_config_shared
 print(" ".join(configure_cmd))
 
-subprocess.check_call(configure_cmd, cwd=BUILD_SHARED_PATH.absolute().as_posix(), env=env)
-subprocess.check_call(['cmake', '--build', '.', '--target', "all"] + build_args, cwd=BUILD_SHARED_PATH.absolute().as_posix(), env=env)
+subprocess.check_call(configure_cmd, cwd=BUILD_SHARED_PATH.resolve().as_posix(), env=env)
+subprocess.check_call(['cmake', '--build', '.', '--target', "all"] + build_args, cwd=BUILD_SHARED_PATH.resolve().as_posix(), env=env)
 
 
-subprocess.check_call(configure_cmd, cwd=BUILD_STATIC_PATH.absolute().as_posix(), env=env)
-subprocess.check_call(['cmake', '--build', '.', '--target', "all"] + build_args, cwd=BUILD_STATIC_PATH.absolute().as_posix(), env=env)
+subprocess.check_call(configure_cmd, cwd=BUILD_STATIC_PATH.resolve().as_posix(), env=env)
+subprocess.check_call(['cmake', '--build', '.', '--target', "all"] + build_args, cwd=BUILD_STATIC_PATH.resolve().as_posix(), env=env)
+
+subprocess.check_call([CPACK_BIN, '--config', CPACK_CONFIG_PATH.resolve().as_posix()], cwd=BUILD_PATH.resolve().as_posix(), env=env)
 
