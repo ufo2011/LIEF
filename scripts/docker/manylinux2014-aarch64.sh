fac09cf22f82d9970a552fe88057ce16cb4ce431
Romain Thomas <me@romainthomas.fr>
2021-01-02 08:45:31 +0100

    WIP

--- scripts/docker/manylinux2014-aarch64.sh
+++ scripts/docker/manylinux2014-aarch64.sh
@@ -3,6 +3,7 @@ set -ex
 CXXFLAGS='-static-libgcc -static-libstdc++' \
 $PYTHON_BINARY setup.py --ninja \
   build -t /tmp bdist_wheel \
-  --plat-name manylinux2014_aarch64 \
-  chown -R 1000:1000 /work/dist && \
-  chown -R 1000:1000 /work/build
+  --plat-name manylinux2014_aarch64
+
+chown -R 1000:1000 /work/dist
+chown -R 1000:1000 /work/build
