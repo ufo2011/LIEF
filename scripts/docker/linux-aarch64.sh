cbd9f1793f118774ad6cdc83fd7669e7ec000f18
Romain Thomas <me@romainthomas.fr>
2021-01-02 21:10:03 +0100

    WIP

--- scripts/docker/linux-aarch64.sh
+++ scripts/docker/linux-aarch64.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/bash
-
 set -ex
 
-export CXXFLAGS='-ffunction-sections -fdata-sections -fvisibility-inlines-hidden -static-libgcc -static-libstdc++'
-export CFLAGS='-ffunction-sections -fdata-sections -static-libgcc -static-libstdc++'
+# Script to be run with liefproject/manylinux2014-aarch64:
+# ==============================================================================================
+# docker run \
+#  -e CCACHE_DIR=/ccache \
+#  -v $LIEF_SRC:/work \
+#  -v $HOME/.ccache:/ccache \
+#  --rm liefproject/manylinux2014-aarch64:base bash /work/scripts/docker/linux-aarch64.sh
+# ==============================================================================================
+#
+
+CXXFLAGS='-ffunction-sections -fdata-sections -fvisibility-inlines-hidden -static-libgcc -static-libstdc++'
+CFLAGS='-ffunction-sections -fdata-sections -static-libgcc -static-libstdc++'
 export LDFLAGS='-Wl,--gc-sections -Wl,--exclude-libs,ALL'
 
 ARCH_DIR="linux-aarch64"
@@ -12,6 +21,8 @@ mkdir -p build/$ARCH_DIR/static-release && mkdir -p build/$ARCH_DIR/shared-relea
 pushd build/$ARCH_DIR/shared-release
 
 cmake ../../.. -GNinja \
+  -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
+  -DCMAKE_C_FLAGS="$CFLAGS" \
   -DCMAKE_LINK_WHAT_YOU_USE=on \
   -DBUILD_SHARED_LIBS=on \
   -DLIEF_PYTHON_API=off \
@@ -24,6 +35,8 @@ popd
 pushd build/$ARCH_DIR/static-release
 
 cmake ../../.. -GNinja \
+  -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
+  -DCMAKE_C_FLAGS="$CFLAGS" \
   -DCMAKE_LINK_WHAT_YOU_USE=on \
   -DBUILD_SHARED_LIBS=off \
   -DLIEF_PYTHON_API=off \
@@ -40,4 +53,4 @@ popd
 
 /bin/mv build/$ARCH_DIR/*.tar.gz build/
 
-chown -R 1000:1000 build/$ARCH_DIR
+chown -R 1000:1000 build/
--- scripts/docker/manylinux2014-aarch64.sh
+++ scripts/docker/manylinux2014-aarch64.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/bash
+
+# Script to be run with liefproject/manylinux2014-aarch64. Example with Python 3.9:
+# ==============================================================================================
+# docker run \
+#  -e CCACHE_DIR=/ccache \
+#  -e PYTHON_BINARY=/opt/python/cp39-cp39/bin/python3.9 \
+#  -v $LIEF_SRC:/work \
+#  -v $HOME/.ccache:/ccache \
+#  --rm liefproject/manylinux2014-aarch64:base bash /work/scripts/docker/manylinux2014-aarch64.sh
+# ==============================================================================================
+#
 set -ex
 CXXFLAGS='-static-libgcc -static-libstdc++' \
 $PYTHON_BINARY setup.py --ninja \
